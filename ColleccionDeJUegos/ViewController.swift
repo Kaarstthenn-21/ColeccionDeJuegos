de845b4d32eebb336e66b2db55517383575d8472
Kaarstthenn Ancco Escobar <kaarstthen.ancco@tecsup.edu.pe>
2021-10-13 12:34:32 -0500

    first commit

---
+++ ColleccionDeJUegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColleccionDeJUegos
+//
+//  Created by Mac 05 on 10/13/21.
+//  Copyright © 2021 Kaarstthen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
